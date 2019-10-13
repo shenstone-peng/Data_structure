f7c4eccbd63f9b3cb35851ade852e2ead0b80761
ShenstoneKyoto <489439220@qq.com>
2019-10-13 14:40:45 +0800

    更新sort算法

--- sort/sort.cpp
+++ sort/sort.cpp
@@ -16,7 +16,7 @@ using namespace std;
 class Solution{
 public:
     void bubbleSort(vector<int> A){
-        int size=A.size();
+        int size=(int)A.size();
         bool flag=false;
         for (int i=0; i<size; i++) {
             flag=false;
@@ -32,12 +32,29 @@ public:
             if(!flag)break;
         }
     }
+    void insertionSort(vector<int> A){
+        int size=(int)A.size();
+        if(size<=1) return ;
+        for(int i=1;i<size;i++){
+            int tmp=A[i];
+            int j=i-1;
+            for(;j>=0;j--){
+                if(A[j]>tmp){
+
+                    A[j+1]=A[j];
+
+                }
+                else break;
+            }
+            A[j+1]=tmp;
+        }
+    }
 };
 int main(int argc, char const *argv[]) {
     printf("hello");
     int a[]={1,3,5,2,43,21,45,32,12,3};
     vector<int> test(a,a+sizeof(a)/sizeof(int));
     Solution* sort=new Solution;
-    sort->bubbleSort(test);
+    sort->insertionSort(test);
     return 0;
 }
